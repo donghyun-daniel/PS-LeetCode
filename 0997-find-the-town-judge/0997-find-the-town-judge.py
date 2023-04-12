286f1f412f5f0aea3fadd3459c7ddd789fb041ff
donghyun-daniel <donghyun.main@gmail.com>
2023-04-12 13:27:33 +0900

    Time: 715 ms (96.39%), Space: 18.9 MB (89.11%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,20 +1,21 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        judge_cands = {i+1 for i in range(n)}
-        required_trust_cnt = {i+1: 0 for i in range(n)}
-        
+        from collections import defaultdict
+        cands = defaultdict(int)
+        for i in range(n):
+            cands[i+1] = 0
         for [f, t] in trust:
-            if f in judge_cands:
-                judge_cands.remove(f)
-            required_trust_cnt[t] += 1
-        if len(judge_cands) == 1:
-            judge_cand = judge_cands.pop()
-            if required_trust_cnt[judge_cand] == n-1:
-                return judge_cand
-            else:
-                return -1
+            if f in cands:
+                cands.pop(f)
+            if t in cands:
+                cands[t] += 1
+        if cands:
+            for cand in cands:
+                if cands[cand] == n-1:
+                    return cand
+                else:
+                    return -1
         else:
             return -1
             
-            
-            
+        
