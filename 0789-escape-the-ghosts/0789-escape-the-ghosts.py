6d5dbcdb3af0e629b8d36654fede9806606586cc
donghyun-daniel <donghyun.main@gmail.com>
2023-09-18 19:26:04 +0900

    Time: 56 ms (37.59%), Space: 16.3 MB (77.44%) - LeetHub

--- 0789-escape-the-ghosts/0789-escape-the-ghosts.py
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.py
@@ -2,10 +2,6 @@ class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
         distance = abs(target[0]) + abs(target[1])
         for (x, y) in ghosts:
-            print(abs(x - target[0]) + abs(y - target[1]), distance)
             if abs(x - target[0]) + abs(y - target[1]) <= distance:
                 return False
         return True
-            
-        
-        
