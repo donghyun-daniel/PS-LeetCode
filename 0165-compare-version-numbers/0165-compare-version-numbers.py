fd2b0bb3d873ee6e0a506189088e48d1626fe048
donghyun-daniel <donghyun.main@gmail.com>
2023-10-29 23:38:08 +0900

    Time: 40 ms (44.76%), Space: 16.2 MB (82.85%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -1,12 +1,10 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
         from itertools import zip_longest
-        v1, v2 = version1.split("."), version2.split(".")
+        v1 = list(map(int, version1.split("."))) 
+        v2 = list(map(int, version2.split(".")))
         for r1, r2 in zip_longest(v1, v2, fillvalue=0):
-            if int(r1) < int(r2):
-                return -1
-            elif int(r1) > int(r2):
-                return 1
-            else:
-                continue
+            if r1 == r2:
+                  continue
+            else: return -1 if r1 < r2 else 1
         return 0
