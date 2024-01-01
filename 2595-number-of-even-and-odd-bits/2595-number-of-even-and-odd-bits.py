53ae0db48953d2adb2ecc61ea2e4edfe7eec84b5
donghyun-daniel <donghyun.main@gmail.com>
2024-01-01 21:55:29 +0900

    Time: 33 ms (91.97%), Space: 17.4 MB (6.33%) - LeetHub

--- 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.py
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.py
@@ -1,10 +1,10 @@
 class Solution:
     def evenOddBit(self, n: int) -> List[int]:
         ans = [0,0]
-        cnt = 0
+        flag = False
         while n:
             val = n & 1
             n >>= 1
-            ans[cnt] += val
-            cnt = (cnt + 1) % 2
+            ans[flag] += val
+            flag = not flag
         return ans
