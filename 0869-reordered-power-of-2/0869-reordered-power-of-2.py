b41e2528bf5504223f38d24990c46bc3a8c9318f
donghyun-daniel <donghyun.main@gmail.com>
2024-01-15 22:29:50 +0900

    Time: 31 ms (93.22%), Space: 17.4 MB (13.98%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.py
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.py
@@ -3,7 +3,7 @@ class Solution:
         arr = []
         comparator = sorted(str(n))
         power_of_two = 1
-        for _ in range(0, 31):
+        for _ in range(30):
             if comparator == sorted(str(power_of_two)):
                 return True
             power_of_two *= 2
