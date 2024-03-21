fd0c56f3f0980ff2bacb39266dd43bffbe8bfcf1
Daniel(Donghyun) Sung <donghyun.main@gmail.com>
2024-03-21 10:57:40 +0900

    Time: 98 ms (55.74%), Space: 17.7 MB (85.25%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.py
+++ 0908-smallest-range-i/0908-smallest-range-i.py
@@ -1,4 +1,4 @@
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
-        min_, max_ = min(nums) + k , max(nums) - k
-        return max_ - min_ if max_ > min_ else 0
+        min_val, max_val = min(nums) + k , max(nums) - k
+        return max_val - min_val if max_val > min_val else 0
