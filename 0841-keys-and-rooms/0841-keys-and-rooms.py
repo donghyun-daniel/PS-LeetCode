e6d5a55d2c87ddc9341959fabb3d009995d21f11
Daniel(Donghyun) Sung <donghyun.main@gmail.com>
2024-03-21 10:50:24 +0900

    Time: 67 ms (48.54%), Space: 17.1 MB (62.20%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,9 +1,11 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        visited = {i: False for i in range(len(rooms))}
+        visited = [True] + [False] * (len(rooms) - 1)
         queue = [0]
         while queue:
             now = queue.pop()
-            visited[now] = True
-            queue += [key for key in rooms[now] if not visited[key]]
-        return all(visited.values())
+            not_visited_rooms = [key for key in rooms[now] if not visited[key]]
+            for key in not_visited_rooms:
+                visited[key] = True
+            queue += not_visited_rooms
+        return all(visited)
