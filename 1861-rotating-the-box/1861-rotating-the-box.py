583e4c3d0f40d36147559258a411af1f43229af5
Daniel(Donghyun) Sung <donghyun.main@gmail.com>
2024-04-10 19:43:52 +0900

    Time: 1880 ms (64.10%), Space: 29.1 MB (25.93%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -1,8 +1,6 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         new_box = [list(e) for e in zip(*box[::-1])]  # 90 degrees clockwise
-        for r in new_box:
-            print(r)
         (rows, cols) = (len(new_box), len(new_box[0]))
         for c in range(cols):
             target_row = rows - 1
