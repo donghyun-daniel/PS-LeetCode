9893fd195414cff051cc74bc59204061000be59b
Daniel(Donghyun) Sung <donghyun.main@gmail.com>
2024-04-10 19:42:45 +0900

    Time: 1912 ms (41.31%), Space: 29.1 MB (14.25%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -1,20 +1,17 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
-        def rotate(mat: List[List[str]]):
-            return [list(e) for e in zip(*mat[::-1])]
-        def apply_gravity(mat):
-            rows, cols = len(mat), len(mat[0])
-            for c in range(cols):
-                for r in reversed(range(rows)):
-                    if mat[r][c] == '#':
-                        target_row = None
-                        for empty_row in range(r+1, rows):
-                            if mat[empty_row][c] == '.':
-                                target_row = empty_row
-                            else: break
-                        if target_row:
-                            mat[r][c], mat[target_row][c] = mat[target_row][c], mat[r][c]
-            return mat
-
-        rotated_box = rotate(box)
-        return apply_gravity(rotated_box)
+        new_box = [list(e) for e in zip(*box[::-1])]  # 90 degrees clockwise
+        for r in new_box:
+            print(r)
+        (rows, cols) = (len(new_box), len(new_box[0]))
+        for c in range(cols):
+            target_row = rows - 1
+            for r in reversed(range(rows)):
+                if new_box[r][c] == '#':
+                    new_box[r][c], new_box[target_row][c] = '.', '#'
+                    target_row -= 1
+                elif new_box[r][c] == '*':
+                    target_row = r - 1
+                elif new_box[r][c] == '.':
+                    target_row = max(target_row, r)
+        return new_box
