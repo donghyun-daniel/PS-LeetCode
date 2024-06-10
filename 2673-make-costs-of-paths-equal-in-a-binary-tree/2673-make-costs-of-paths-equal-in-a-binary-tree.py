7169b08d7df78d02079fe57f9c59141c148529e6
Daniel(Donghyun) Sung <donghyun.main@gmail.com>
2024-06-10 13:17:48 +0900

    Time: 954 ms (82.48%), Space: 26 MB (60.58%) - LeetHub

---
+++ 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minIncrements(self, n: int, cost: List[int]) -> int:
+        result = 0
+        for idx in range(n-2, 0, -2):
+            result += abs(cost[idx] - cost[idx+1])
+            cost[(idx//2)] += max(cost[idx:idx+2])
+        return result
+            
+        
