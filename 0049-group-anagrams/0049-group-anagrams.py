7c5d4b80de9eee9d6a06460f1bdcbcc84df87f8d
Daniel(Donghyun) Sung <donghyun.main@gmail.com>
2024-06-16 19:41:44 +0900

    Time: 78 ms (95.00%), Space: 19.9 MB (62.92%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, words: List[str]) -> List[List[str]]:
+        anagram = defaultdict(list)
+        for word in words:
+            anagram[''.join(sorted(word))].append(word)
+        return anagram.values()
+            
+        
